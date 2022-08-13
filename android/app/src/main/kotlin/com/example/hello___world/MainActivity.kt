6f7bec5ddfe99fa2767dac42e4c44eb0b2e79e56
truongkutetk97 <lenhattruong.tk@gmail.com>
2022-08-13 17:24:35 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/hello___world/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hello___world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome tooo Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello '),
+        ),
+      ),
+    );
+  }
+}
